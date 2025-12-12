930b2bf79cc11df3f0b8ab667db3b1080cf66f14
roundRekt <roundrekt@icloud.com>
2025-12-12 23:22:43 +0800

    添加 SdkUrlRewriter 类用于重写 SDK URL

--- README.md
+++ README.md
@@ -1,33 +1,48 @@
 # Blue-Archive-URL-Rewriter
 
 <div align="center">
-自动解密、获取并修改「ブルーアーカイブ」APK 中的 ServerInfoDataUrl，以实现汉化等目的。</div>
-
+自动解密、获取并修改「ブルーアーカイブ」APK 中的 ServerInfoDataUrl 和 Sdk_Url，以实现汉化、加速等目的。</div>
 
 ## 用途 / Usage
 
 ```python
 from rewrite_url import ServerInfoUrlRewriter
+from rewrite_url import SdkUrlRewriter
 
 ServerInfoUrlRewriter().fast_rewrite("/path/to/your/directory", "example.com") # 根据路径和目标 URL 自动修改文件
+SdkUrlRewriter().fast_rewrite("/path/to/your/directory", "example.com") # 根据路径和目标 URL 自动修改文件
 
-#rewriter = ServerInfoUrlRewriter()
+#serverinfourlrewriter = ServerInfoUrlRewriter()
 
-#rewriter.get_gamemainconfig(path) # 获取 GameMainConfig 并保存到 self.original_config
+#serverinfourlrewriter.get_gamemainconfig(path) # 获取 GameMainConfig 并保存到 self.original_config
 
-#rewriter.get_serverinfo_url() # 根据 GameMainConfig 解析 ServerInfo URL 并保存到 self.original_url
-#rewriter.get_serverinfo_url(original_config) # 可输入字符串类型的 Base64 编码的加密 GameMainConfig
+#serverinfourlrewriter.get_serverinfo_url() # 根据 GameMainConfig 解析 ServerInfo URL 并保存到 self.original_url
+#serverinfourlrewriter.get_serverinfo_url(original_config) # 可输入字符串类型的 Base64 编码的加密 GameMainConfig
 
-#rewriter.modify_serverinfo_url(target_url) # 修改 ServerInfo URL 并保存到 self.modified_url
-#rewriter.modify_serverinfo_url(target_url, original_url) # 可输入自定义原始 ServerInfo URL
+#serverinfourlrewriter.modify_serverinfo_url(target_url) # 修改 ServerInfo URL 并保存到 self.modified_url
+#serverinfourlrewriter.modify_serverinfo_url(target_url, original_url) # 可输入自定义原始 ServerInfo URL
 
-#rewriter.modify_config() # 根据修改后的 ServerInfo URL 修改 GameMainConfig 并保存到 self.modified_config
-#rewriter.modify_config(original_config, modified_url) # 可选输入字符串类型的 Base64 编码的加密 GameMainConfig 和修改后的 ServerInfo URL
+#serverinfourlrewriter.modify_config() # 根据修改后的 ServerInfo URL 修改 GameMainConfig 并保存到 self.modified_config
+#serverinfourlrewriter.modify_config(original_config, modified_url) # 可选输入字符串类型的 Base64 编码的加密 GameMainConfig 和修改后的 ServerInfo URL
 
-#rewriter.rewrite_config(path) # 根据修改后的 GameMainConfig 写回文件
-#rewriter.rewrite_config(path, modified_config) # 可选输入字符串类型的 Base64 编码的加密 GameMainConfig
+#serverinfourlrewriter.rewrite_config(path) # 根据修改后的 GameMainConfig 写回文件
+#serverinfourlrewriter.rewrite_config(path, modified_config) # 可选输入字符串类型的 Base64 编码的加密 GameMainConfig
+
+#sdkurlrewriter = SdkUrlRewriter()
+
+#sdkurlrewriter.get_sdkconfigsettings(path) # 获取 SDKConfigSettings 并保存到 self.original_config
+
+#sdkurlrewriter.get_sdk_url() # 根据 SDKConfigSettings 解析 SDK URL 并保存到 self.original_url
+#sdkurlrewriter.get_sdk_url(original_config) # 可输入字符串类型的 SDKConfigSettings
+
+#sdkurlrewriter.modify_sdk_url(target_url) # 修改 SDK URL 并保存到 self.modified_url
+
+#sdkurlrewriter.modify_config() # 根据修改后的 SDK URL 修改 SDKConfigSettings 并保存到 self.modified_config
+#sdkurlrewriter.modify_config(original_config, modified_url) # 可选输入字符串类型的 SDKConfigSettings 和修改后的 SDK URL
+
+#sdkurlrewriter.rewrite_config(path) # 根据修改后的 SDKConfigSettings 写回文件
+#sdkurlrewriter.rewrite_config(path, modified_config) # 可选输入字符串类型的 SDKConfigSettings
 ```
-
 
 ## 关于 / About
 
--- rewrite_url.py
+++ rewrite_url.py
@@ -54,7 +54,7 @@ class ServerInfoUrlRewriter:
         print("已生成修改后的 GameMainConfig")
         return encrypted_config
 
-    def rewrite_config(self, path, modified_config = None): # 根据修改后的 GameMainConfig 写回二进制文件
+    def rewrite_config(self, path, modified_config = None): # 根据修改后的 GameMainConfig 写回文件
         if modified_config is None: modified_config = self.modified_config
         files = []
         for filename in os.listdir(path):
@@ -81,28 +81,111 @@ class ServerInfoUrlRewriter:
         self.modify_config()
         self.rewrite_config(path)
 
+class SdkUrlRewriter:
+    def get_sdkconfigsettings(self, path): # 获取 SDKConfigSettings 并保存到 self.original_config
+        files = []
+        for filename in os.listdir(path):
+            filepath = os.path.join(path, filename)
+            if os.path.isfile(filepath) and "." not in filename:
+                files.append(filepath)
+        for file in files:
+            env = UnityPy.load(file)
+            for obj in env.objects:
+                if obj.type.name == "TextAsset":
+                    txt = obj.parse_as_object()
+                    if txt.m_Name != "SDKConfigSettings":
+                        continue
+                    original_config = txt.m_Script.encode("utf-8", "surrogateescape")
+                    self.original_config = original_config.decode("utf-8", "surrogateescape")
+                    return original_config
+        
+    def get_sdk_url(self, original_config = None): # 根据 SDKConfigSettings 解析 SDK URL 并保存到 self.original_url
+        if original_config is None: original_config = self.original_config
+        config_json = json.loads(original_config)
+        original_url = config_json["Regions"]["Jp"]["Sdk_Url"]
+        self.original_url = original_url
+        print(f"已获取的 SDK URL: {original_url}")
+        return original_url
+
+    def modify_sdk_url(self, target_url): # 修改 SDK URL 并保存到 self.modified_url
+        modified_url = target_url
+        if not urlparse(target_url).scheme: # 补全协议，默认https
+            modified_url= "https://" + modified_url
+        self.modified_url = modified_url
+        print(f"修改后的 SDK URL: {modified_url}")
+        return modified_url
+
+    def modify_config(self, original_config = None, modified_url = None): # 根据修改后的 SDK URL 修改 SDKConfigSettings 并保存到 self.modified_config
+        if original_config is None: original_config = self.original_config
+        if modified_url is None: modified_url = self.modified_url
+        config_json = json.loads(original_config)
+        config_json["Regions"]["Jp"]["Sdk_Url"] = modified_url
+        modified_config = json.dumps(config_json, indent=1, ensure_ascii=False)
+        self.modified_config = modified_config
+        print("已生成修改后的 SDKConfigSettings")
+        return modified_config
+
+    def rewrite_config(self, path, modified_config = None): # 根据修改后的 SDKConfigSettings 写回文件
+        if modified_config is None: modified_config = self.modified_config
+        files = []
+        for filename in os.listdir(path):
+            filepath = os.path.join(path, filename)
+            if os.path.isfile(filepath) and "." not in filename:
+                files.append(filepath)
+        for file in files:
+            env = UnityPy.load(file)
+            for obj in env.objects:
+                if obj.type.name == "TextAsset":
+                    txt = obj.parse_as_object()
+                    if txt.m_Name != "SDKConfigSettings":
+                        continue
+                    txt.m_Script = modified_config
+                    txt.save()
+                    with open(file, "wb") as f:
+                        f.write(env.file.save())
+        print(f"已写入修改后的 SDKConfigSettings")
+
+    def fast_rewrite(self, path, target_url): # 根据路径和目标 URL 自动修改文件
+        self.get_sdkconfigsettings(path)
+        self.get_sdk_url()
+        self.modify_sdk_url(target_url)
+        self.modify_config()
+        self.rewrite_config(path)
+        
 if __name__ == "__main__":
     ServerInfoUrlRewriter().fast_rewrite("/path/to/your/directory", "example.com") # 根据路径和目标 URL 自动修改文件
+    SdkUrlRewriter().fast_rewrite("/path/to/your/directory", "example.com") # 根据路径和目标 URL 自动修改文件
 
     #path = "/path/to/your/directory" # 二进制文件所在目录
-    #target_url = "https://example.com/" # 目标 ServerInfo URL，可省略协议和末尾斜杠，如example.com
-    #original_url = "https://yostar-serverinfo.bluearchiveyostar.com/r86_63_valo0kvoir39ti2g00u7.json"
-    #modified_url = "https://example.com/r86_63_valo0kvoir39ti2g00u7.json"
-    #original_config = "5N1TBo9y+ldaI2cGzHKTV20jZAbucvdXbCNCBp5yg1cvI3oG7nL1VyMjZAaZcoFXVSNtBulyr1deI2QGm3K1V20jWwbkcuBXISMKBtVyk1dUI3kG/HK0V30jawaYcoNXcSNvBuxytVcmIxUGj3LuVzkjXwbhcvZXXiN/Bt5yhldtIxAG/HKaVy4jXgbKcpBXeiNRBshy7VdhI2oG/HL/VyYjCgaXcuBXYyNaBplylFdMI08GkHL/VzkjBAaPcppXKyMcBvRymldZI24G3HKmVygjcgbdcpZXfCMRBs5yiVd2I1gG23KmV3YjWAbicodXdyNfBsNyo1d2I2oGn3KnV14jHAbOcppXXyNyBtxyoVcoI3IGynKWV3wjFQaQcuBXISMKBpVy9VdaI34G9XKAV38jWQbJcppXQSMQBvlypVcuI0sG+nKVV3QjXQbJcqtXVSNnBu9yhFdsI2UGzHKsV1EjGgbIcodXLyNLBvVygFddI1kG4XKKV0EjBwb5cqpXcyNLBuhylVdrIxsGyXKvV3cjZwbpcvNXbCNjBsxyr1ciIxoGzHKpVy8jewb1codXKiNZBvRy8VdBI08G+XKlV3MjSwbvcq9XayNPBslyrFcuI2cG5XKuV2wjZAbMcq9XcyMaBshyqVcvI3EG9XKAV2sjWQbPcqxXQSMHBvlypVd3I0sG7HKvV2sjUgbJcqtXUSNnBuVyrldsI2UGzHKvV2MjGgbkcvJXLyN4BvVyhFdvI1kG43LxV0EjfAb5cqlXbyNLBvhyhVdrI00GyXKsV2sjZwblcoRXbCN4Bsxyr1cuIxoG/XKHVy8jfwb1coBXTSNZBs9yrFdBI0QG+XKlVyIjSwb4coVXdCMcBslyrFdzI2cG63KEV2MjegbMcq9XQSMaBv1yh1cuI2YG9XKAV0EjWQbjcqxXQiNBBvlyqld/I0sG6HKFV2sjXQbJcq9XUSNnBo9y7lc5I1wG/nKwV30jSgaacrpXcyN5Bv9yiVdeI2MG2XKYV20jWgbAcoRXcSNtBt1y9ldqIxkG6nLpVysjcQb4cpdXSCNDBsRysFdUI0oGmnKMV3MjfAbVcolXfSNjBtty81dtI1kG6nKEV0sjbQbCcpNXaiNRBsBy+lcmIwoGl3LgV3QjewbfcotXeSMfBvtyqldJI2oG4XKhV1AjXgbZcrRXaSMaButyh1deI1gGynKzV2EjRQaVcv9XOSNVBg=="
-    #modified_config = "5N1TBo9y+ldaI2cGzHKTV20jZAbucvdXbCNCBp5yg1cvI3oG7nL1VyMjZAaZcoFXVSNtBulyr1deI2QGm3K1V20jWwbkcuBXISMKBtVyk1dUI3kG/HK0V30jawaYcoNXcSNvBuxytVcmIxUGj3LuVzkjXwbhcvZXXiN/Bt5yhldtIxAG/HKaVy4jXgbKcpBXeiNRBshy7VdhI2oG/HL/VyYjCgaXcuBXYyNaBplylFdMI08GkHL/VzkjBAaPcppXKyMcBvRymldZI24G3HKmVygjcgbdcpZXfCMRBs5yiVd2I1gG23KmV3YjWAbicodXdyNfBsNyo1d2I2oGn3KnV14jHAbOcppXXyNyBtxyoVcoI3IGynKWV3wjFQaQcuBXISMKBpVy9VdaI34G9XKAV38jWQbJcppXQSMQBvlypVcuI0sG+nKVV3QjXQbJcqtXVSNnBupyhFdsI0oGzHKvV1kjGgb0cpdXLyNmBvVyg1ciI1kG93KKV0IjQQb5cqpXLiNLBulyhVdrI1sGyXKrV1UjZwbpcvNXYyNKBsxyqFd/IxoG+HLyVy4jZAb1coRXWSNZBvVyrFdBIx4G+XKqV2MjSwbpcvBXayNdBslyqFdjI2cG63KuV2wjfgbMcq9XLiMaBvdyl1cvI3gG9XKEV1kjWQbicopXQSMcBvlyqldJI0sG+HKVV2sjRQbJcqhXYyNnBvlylFdsI38GzHKoV0EjGgbkcqlXLyNwBvVygFdZI1kGz3KsV0EjQQb5cqVXJiMVBo9y7lc5I1wG/nKwV30jSgaacrpXcyN5Bv9yiVdeI2MG2XKYV20jWgbAcoRXcSNtBt1y9ldqIxkG6nLpVysjcQb4cpdXSCNDBsRysFdUI0oGmnKMV3MjfAbVcolXfSNjBtty81dtI1kG6nKEV0sjbQbCcpNXaiNRBsBy+lcmIwoGl3LgV3QjewbfcotXeSMfBvtyqldJI2oG4XKhV1AjXgbZcrRXaSMaButyh1deI1gGynKzV2EjRQaVcv9XOSNVBg=="
+    #target_url = "https://example.com/" # 目标 URL，可省略协议和末尾斜杠，如example.com
 
-    #rewriter = ServerInfoUrlRewriter()
+    #serverinfourlrewriter = ServerInfoUrlRewriter()
 
-    #rewriter.get_gamemainconfig(path) # 获取 GameMainConfig 并保存到 self.original_config
+    #serverinfourlrewriter.get_gamemainconfig(path) # 获取 GameMainConfig 并保存到 self.original_config
 
-    #rewriter.get_serverinfo_url() # 根据 GameMainConfig 解析 ServerInfo URL 并保存到 self.original_url
-    #rewriter.get_serverinfo_url(original_config) # 可输入字符串类型的 Base64 编码的加密 GameMainConfig
+    #serverinfourlrewriter.get_serverinfo_url() # 根据 GameMainConfig 解析 ServerInfo URL 并保存到 self.original_url
+    #serverinfourlrewriter.get_serverinfo_url(original_config) # 可输入字符串类型的 Base64 编码的加密 GameMainConfig
 
-    #rewriter.modify_serverinfo_url(target_url) # 修改 ServerInfo URL 并保存到 self.modified_url
-    #rewriter.modify_serverinfo_url(target_url, original_url) # 可输入自定义原始 ServerInfo URL
+    #serverinfourlrewriter.modify_serverinfo_url(target_url) # 修改 ServerInfo URL 并保存到 self.modified_url
+    #serverinfourlrewriter.modify_serverinfo_url(target_url, original_url) # 可输入自定义原始 ServerInfo URL
 
-    #rewriter.modify_config() # 根据修改后的 ServerInfo URL 修改 GameMainConfig 并保存到 self.modified_config
-    #rewriter.modify_config(original_config, modified_url) # 可选输入字符串类型的 Base64 编码的加密 GameMainConfig 和修改后的 ServerInfo URL
+    #serverinfourlrewriter.modify_config() # 根据修改后的 ServerInfo URL 修改 GameMainConfig 并保存到 self.modified_config
+    #serverinfourlrewriter.modify_config(original_config, modified_url) # 可选输入字符串类型的 Base64 编码的加密 GameMainConfig 和修改后的 ServerInfo URL
 
-    #rewriter.rewrite_config(path) # 根据修改后的 GameMainConfig 写回文件
-    #rewriter.rewrite_config(path, modified_config) # 可选输入字符串类型的 Base64 编码的加密 GameMainConfig
+    #serverinfourlrewriter.rewrite_config(path) # 根据修改后的 GameMainConfig 写回文件
+    #serverinfourlrewriter.rewrite_config(path, modified_config) # 可选输入字符串类型的 Base64 编码的加密 GameMainConfig
+
+    #sdkurlrewriter = SdkUrlRewriter()
+
+    #sdkurlrewriter.get_sdkconfigsettings(path) # 获取 SDKConfigSettings 并保存到 self.original_config
+
+    #sdkurlrewriter.get_sdk_url() # 根据 SDKConfigSettings 解析 SDK URL 并保存到 self.original_url
+    #sdkurlrewriter.get_sdk_url(original_config) # 可输入字符串类型的 SDKConfigSettings
+
+    #sdkurlrewriter.modify_sdk_url(target_url) # 修改 SDK URL 并保存到 self.modified_url
+
+    #sdkurlrewriter.modify_config() # 根据修改后的 SDK URL 修改 SDKConfigSettings 并保存到 self.modified_config
+    #sdkurlrewriter.modify_config(original_config, modified_url) # 可选输入字符串类型的 SDKConfigSettings 和修改后的 SDK URL
+
+    #sdkurlrewriter.rewrite_config(path) # 根据修改后的 SDKConfigSettings 写回文件
+    #sdkurlrewriter.rewrite_config(path, modified_config) # 可选输入字符串类型的 SDKConfigSettings
